a0c72ba318653df84becccbb41cbd458e57f3a44
Endel Guimaraes Silva <endel.gs@gmail.com>
2015-12-17 09:11:02 -0200

    Fim da sprint 1

--- themes/store/framework/layouts/content-grid.php
+++ themes/store/framework/layouts/content-grid.php
@@ -5,7 +5,6 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('col-md-12 col-sm-12 grid'); ?>>
-
 		<div class="featured-thumb col-md-4 col-sm-4">
 			<?php if (has_post_thumbnail()) : ?>	
 				<a href="<?php the_permalink() ?>" title="<?php the_title() ?>"><?php the_post_thumbnail('pop-thumb'); ?></a>
@@ -17,8 +16,20 @@
 		<div class="out-thumb col-md-8 col-sm-8">
 			<header class="entry-header">
 				<h1 class="entry-title title-font"><a class="hvr-underline-reveal" href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
-				<div class="postedon"><?php store_posted_on(); ?></div>
-				<span class="entry-excerpt"><?php echo substr(get_the_excerpt(),0,200).(get_the_excerpt() ? "..." : "" ); ?></span>
+				<div class="postedon">
+					<?php 
+						$disponivel = get_field('disponivel_em');
+						$array_disponivel = array();
+						if(is_array($disponivel)){
+							echo "Disponível em: ";
+							foreach($disponivel as $item){
+								$array_disponivel = apply_filters('the_title',$item->post_title);
+							}
+							echo (is_array($array_disponivel))?implode($array_disponivel):$array_disponivel;
+							echo " - (R$".get_field('preco').")";
+						}
+					?>
+				</div>
 				<span class="readmore"><a class="hvr-underline-from-center" href="<?php the_permalink() ?>"><?php _e('Read More','store'); ?></a></span>
 			</header><!-- .entry-header -->
 		</div><!--.out-thumb-->
